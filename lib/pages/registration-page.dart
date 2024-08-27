caf29581056e883ddad12b81ec8b1a98ea573858
Michele <msa@danskscanning.dk>
2024-08-27 11:18:48 +0200

    Topbar
    added topbar and bg color to registrationpage

--- lib/pages/registration-page.dart
+++ lib/pages/registration-page.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:haenreg_mobile/components/case-item.dart';
+import 'package:haenreg_mobile/components/custom-top-bar.dart';
 
 class RegistrationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Registering!!'),
+     appBar: CustomTopBar(
+        isEditMode: true, // Aktiver redigeringstilstand
+        onEdit: () {
+          // Handling for rediger-knappen
+          print("Rediger-knappen blev klikket");
+        },
       ),
-      body: Center(
-        child: Text('Her kan man registere!!'),
+      body: Container(
+         decoration: const BoxDecoration(
+         gradient: LinearGradient(
+            colors: [
+              Color(0xFFEDF4FA), // #EDF4FA
+              Color(0xFFEBEAE7), // #EBEAE7
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+
       ),
+    ),
+    ),
     );
   }
 }
